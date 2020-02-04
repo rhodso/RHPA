6a24411ba929df0868c799df6c97f7670cc1abfd
rhodso <rhodsoprivate@gmail.com>
2020-02-04 15:25:05 +0000

    Added content to ControlVars.cs
    Added content to ControlVars.cs. Like the last commit we're going a file at a time so that I can tell when it breaks. This is going to be a long and boring couple of hours doing this a file at a time until it either works or breaks. Fortunatley I hate myself enough that I'm prepared to sit here until it does one or the other. Anyway, all praise papa Gates for the continued function of this project

--- RHPA/RHPA/ControlVars.cs
+++ RHPA/RHPA/ControlVars.cs
@@ -4,5 +4,31 @@ using System.Text;
 
 namespace RHPA {
     class ControlVars {
+        //Vars
+        private static bool lightMode;
+        private static Alert myAlert;
+        private static Alert defaultAlert;
+
+        //Getters
+        public bool getLightMode() {
+            return lightMode;
+        }
+        public Alert getAlert() {
+            return myAlert;
+        }
+        public Alert getDefAlert() {
+            return defaultAlert;
+        }
+
+        //Setters
+        public void setLightMode(bool b) {
+            lightMode=b;
+        }
+        public void setAlert(Alert a) {
+            myAlert=a;
+        }
+        public void setDefAlert(Alert a) {
+            defaultAlert=a;
+        }
     }
 }
