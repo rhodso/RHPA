9ce1ad58f9680115dacc7eb25107a65540417f18
rhodso <rhodsoprivate@gmail.com>
2020-02-04 15:22:27 +0000

    Added content to alert.cs
    Added content to alert.cs. Pretty self-explanitory. I've  built it and it works so idk if this broke it or not because it's not calling it yet YAY. All praise to papa Gates for the continued funciton of this project

--- RHPA/RHPA/alert.cs
+++ RHPA/RHPA/alert.cs
@@ -3,6 +3,71 @@ using System.Collections.Generic;
 using System.Text;
 
 namespace RHPA {
-    class alert {
+    class Alert {
+        private double lat;
+        private double lon;
+        private string alertType;
+        private int user;
+        private int proximity;
+        private DateTime exipryTime;
+        private DateTime startTime;
+        private bool active;
+
+        public Alert(double lat,double lon,string type,int proximity,DateTime exipryTime) {
+            this.lat=lat;
+            this.lon=lon;
+            this.alertType=type;
+            this.proximity=proximity;
+            this.exipryTime=exipryTime;
+            this.startTime=DateTime.Now;
+            this.active=getActive();
+        }
+
+        public bool getActive() {
+            bool isActive = TimeSpan.Parse(DateTime.Now.ToString())>TimeSpan.Parse(DateTime.Now.ToString());
+            this.active=isActive;
+            return isActive;
+        }
+        public DateTime GetExipryTime() {
+            return exipryTime;
+        }
+        public DateTime GetStartTime() {
+            return startTime;
+        }
+        public double GetLat() {
+            return lat;
+        }
+        public double GetLon() {
+            return lon;
+        }
+        public string GetAlertType() {
+            return alertType;
+        }
+        public int GetUser() {
+            return user;
+        }
+        public int GetProximity() {
+            return proximity;
+        }
+        public void SetExipryTime(DateTime value) {
+            exipryTime=value;
+        }
+        public void SetLat(double value) {
+            lat=value;
+        }
+        public void SetLon(double value) {
+            lon=value;
+        }
+        public void SetAlertType(string value) {
+            alertType=value;
+        }
+        public void SetUser(int value) {
+            user=value;
+        }
+        public void SetProximity(int value) {
+            proximity=value;
+        }
+
     }
+
 }
